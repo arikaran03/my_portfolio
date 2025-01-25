d56d20cd0c98a093102ce7dbdaf53cbe3ca44f4a
arikaran03 <arikaran258@gmail.com>
2025-01-25 22:36:31 +0530

    first

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    // Wait for the "Hello there" animation to finish
+    setTimeout(() => {
+      // Scroll to the third section after the animation ends
+      const nextSection = document.querySelector('.third-section');
+      nextSection.scrollIntoView({
+        behavior: 'smooth'
+      });
+  
+      // After the scroll, make the navigation bar visible
+      setTimeout(() => {
+        const nav = document.querySelector('#desktop-nav');
+        nav.style.display = 'flex';  // Change display to 'flex' after scroll
+      }, 500);  // Delay before showing the nav bar after scroll
+    }, 2700);  // Duration for the "Hello there" animation
+  };
+  
