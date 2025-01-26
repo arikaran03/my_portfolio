412fabfa03aa3deab1b7d1a1fa768b550b62cc7f
arikaran03 <arikaran258@gmail.com>
2025-01-26 11:13:18 +0530

    Implement navigation styling, logo alignment, and font changes

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 window.onload = function() {
-    // Wait for the "Hello there" animation to finish
+  // Wait for the "Hello there" animation to finish
+  setTimeout(() => {
+    // Scroll to the third section after the animation ends
+    const nextSection = document.querySelector('.third-section');
+    nextSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+
+    // After the scroll, make the navigation bar visible
     setTimeout(() => {
-      // Scroll to the third section after the animation ends
-      const nextSection = document.querySelector('.third-section');
-      nextSection.scrollIntoView({
-        behavior: 'smooth'
-      });
-  
-      // After the scroll, make the navigation bar visible
-      setTimeout(() => {
-        const nav = document.querySelector('#desktop-nav');
-        nav.style.display = 'flex';  // Change display to 'flex' after scroll
-      }, 500);  // Delay before showing the nav bar after scroll
-    }, 2700);  // Duration for the "Hello there" animation
-  };
-  
+      const nav = document.querySelector('#desktop-nav');
+      nav.style.display = 'flex';  // Change display to 'flex' after scroll
+    }, 500);  // Delay before showing the nav bar after scroll
+  }, 2700);  // Duration for the "Hello there" animation
+};
